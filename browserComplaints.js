b4d3bf732c9d7ed3dac70fbad507bbae92445ea9
Tomasz Stachowiak <h3@team0xf.com>
2010-01-01 23:35:48 +0000

    pwetty

--- browserComplaints.js
+++ browserComplaints.js
@@ -2,7 +2,7 @@
 	var browser = navigator.appName;
 	if (browser == "Microsoft Internet Explorer") {
 		var widget = document.getElementById("topStatus");
-		var text = document.createTextNode("This site doesn't support Internet Explorer. Try using a standards-compliant browser, like Firefox or Chrome.");
+		var text = document.createTextNode("This site doesn't support Internet Explorer. Try using a standards-compliant browser, like Chrome or Firefox.");
 		widget.appendChild(text)
 		widget.style.display = "block";
 	}

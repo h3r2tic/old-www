3c897b55654128125a4b1952c0f817ce4b01d12b
Tomasz Stachowiak <h3@h3.gd>
2010-12-16 20:23:08 +0000

    deadlock, pub site, nucleus mods, reel

--- updateSite.sh
+++ updateSite.sh
@@ -1,7 +1,7 @@
 cd output
 
 zip -9 -T siteUpdate `find . -iname '*.html' -or -iname '*_graph*.png' -or -iname '*.jpg'` *.css *.js
-scp siteUpdate.zip h3@team0xf.com:public_html/
+scp siteUpdate.zip h3gdpo@webhost.daily.co.uk:public_html/
 rm siteUpdate.zip
 ssh h3@team0xf.com "cd public_html && unzip -o siteUpdate.zip && rm siteUpdate.zip"
 
